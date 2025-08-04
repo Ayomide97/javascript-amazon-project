03bc781830e3cff6bbf5f8f0a8005fccb33a6c24
Ayomide97 <ayomideolatunde97@gmail.com>
2025-08-04 18:06:57 +0100

    updated the logic for increasing cart quantity

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -34,7 +34,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class="js-quantity-selector-${product.id}">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -84,19 +84,39 @@ document.querySelectorAll('.js-add-to-cart')
 
       });
 
+      const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+      const quantity = Number(quantitySelector.value);
+
       if(matchingItem){
 
-        matchingItem.quantity += 1;
+        matchingItem.quantity += quantity;
 
       } else {
     
-         cart.push({
+        cart.push({
         productId: productId,
-        quantity: 1
+        quantity: quantity
         });
 
       }
 
+      
+      let cartQuantity = 0;
+
+
+      
+      cart.forEach((item) =>{
+
+        cartQuantity += item.quantity;
+      
+
+      });
+
+      document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity;
+
+
+      console.log(cartQuantity);
       console.log(cart)
     });
 
